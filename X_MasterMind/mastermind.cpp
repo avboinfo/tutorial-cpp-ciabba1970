63caf4d55b22f426233a776038165b3b5a7e4010
ciabba1970 <152690688+ciabba1970@users.noreply.github.com>
2024-03-08 14:21:08 +0000

    mi sa che è sbagliato

--- X_MasterMind/mastermind.cpp
+++ X_MasterMind/mastermind.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-#include <time>
-class mastermind
+#include <ctime>
 
+class mastermind
 {
 private:
     int numero_mosse;
-    std::string ultima_mossa; 
- 
+    std::string ultima_mossa;
+
     static const int DIM_GIOCATA_VALIDA = 4;
     int mossa_valida[DIM_GIOCATA_VALIDA];
     int codice_segreto[DIM_GIOCATA_VALIDA];
-    bool sanifica_input(){
+
+    bool sanifica_input()
+    {
         if (DIM_GIOCATA_VALIDA != ultima_mossa.size())
-        return false;
+            return false;
         for (int i = 0; i < ultima_mossa.size(); i++)
-            {
+        {
             char c = ultima_mossa[i];
             if (c >= '0' && c <= '9')
                 mossa_valida[i] = c - '0';
             else if (c == '-') // il trattino verra convertio in -1
                 mossa_valida[i] = -1;
-            else 
+            else
                 return false;
-
-        }    
+        }
         return true;
     }
-    void genera_codice_segreto(){
+
+    void genera_codice_segreto()
+    {
         srand(time(NULL));
         for (int i = 0; i < DIM_GIOCATA_VALIDA; i++)
         {
@@ -37,30 +40,41 @@ private:
     }
 
 public:
-    mastermind(){
+    mastermind()
+    {
         numero_mosse = 0;
         ultima_mossa = "";
         genera_codice_segreto();
-  
-    };
-void nuova_giocata(){
-    do{
-        std::cout << numero_mosse + 1 << " ";
-        std::getline(std::cin, ultima_mossa);  // std:: cin >> ultima mossa; non e i
-    }while (!sanifica_input());
+    }
 
-    numero_mosse++;  
-}
-    void risultato_mossa(){
+    void nuova_giocata()
+    {
+        do
+        {
+            std::cout << numero_mosse + 1 << " ";
+            std::getline(std::cin, ultima_mossa); // std:: cin >> ultima mossa; non e i
+        } while (!sanifica_input());
+
+        numero_mosse++;
+    }
+
+    void risultato_mossa()
+    {
         int cifre_corrette = 0;
         int posizione_corretta = 0;
 
-        for (int i = 0; i < DIM_GIOCATA_VALIDA; i++) {
-            if (mossa_valida[i] == codice_segreto[i]) {
+        for (int i = 0; i < DIM_GIOCATA_VALIDA; i++)
+        {
+            if (mossa_valida[i] == codice_segreto[i])
+            {
                 posizione_corretta++;
-            } else {
-                for (int j = 0; j < DIM_GIOCATA_VALIDA; j++) {
-                    if (mossa_valida[i] == codice_segreto[j]) {
+            }
+            else
+            {
+                for (int j = 0; j < DIM_GIOCATA_VALIDA; j++)
+                {
+                    if (mossa_valida[i] == codice_segreto[j])
+                    {
                         cifre_corrette++;
                         break;
                     }
@@ -68,16 +82,14 @@ void nuova_giocata(){
             }
         }
 
-        if (posizione_corretta == DIM_GIOCATA_VALIDA) {
-            std::cout << "hai indovinato il codice negro." << std::endl;
-        } else {
+        if (posizione_corretta == DIM_GIOCATA_VALIDA)
+        {
+            std::cout << "hai indovinato il codice, bravo!" << std::endl;
+        }
+        else
+        {
             std::cout << "cifre giuste: " << cifre_corrette << std::endl;
             std::cout << "cifre messe giuste: " << posizione_corretta << std::endl;
         }
-}  
-
     }
-
-
 };
-
