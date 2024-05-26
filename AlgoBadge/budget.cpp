feeab1994758b0d7ee1e183705ae0b7f0608c86b
ciabba1970 <152690688+ciabba1970@users.noreply.github.com>
2024-05-26 15:05:04 +0000

    budget parte 2

--- AlgoBadge/budget.cpp
+++ AlgoBadge/budget.cpp
@@ -1,33 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int main() {
+    const int MAX_N = 5000; 
     int n, b;
     cin >> n >> b;
-    int prezzi[n]; 
-    bool Vettore[n] = {false}; 
 
+    int tutti_i_prezzi[MAX_N];
     for (int i = 0; i < n; ++i) {
-        cin >> prezzi[i];
+        cin >> tutti_i_prezzi[i];
     }
+    
+    vector<int> massima_spesa_possibile(b + 1, 0);
+    vector<vector<bool>> used(n, vector<bool>(b + 1, false));
 
-    int spesaTOT = 0;
-    for (int i = 0; i < n; ++i) {
-        for (int j = i; j < n; ++j) {
-            if (!Vettore[j] && spesaTOT + prezzi[j] <= b) {
-                spesaTOT += prezzi[j];
-                Vettore[j] = true; 
+    for (int i = 0; i < n; i++) {
+        for (int j = b; j >= tutti_i_prezzi[i]; j--) {
+            if (massima_spesa_possibile[j - tutti_i_prezzi[i]] + tutti_i_prezzi[i] > massima_spesa_possibile[j]) {
+                massima_spesa_possibile[j] = massima_spesa_possibile[j - tutti_i_prezzi[i]] + tutti_i_prezzi[i];
+                used[i][j] = true;
             }
         }
     }
-
-
-    cout << "I piatti che Gabrie dovrà ordinare per stare nel budget sono:" << endl;
-    for (int i = 0; i < n; ++i) {
-        if (Vettore[i]) {
-            cout << prezzi[i] << " ¥" << endl;
+    
+    vector<int> result;
+    int budget_che_rimane = b;
+    for (int i = n - 1; i >= 0 && budget_che_rimane > 0; i--) {
+        if (used[i][budget_che_rimane]) {
+            result.push_back(tutti_i_prezzi[i]);
+            budget_che_rimane -= tutti_i_prezzi[i];
         }
     }
-
+    
+    for (const int& prezzo : result) {
+        cout << prezzo << endl;
+    }
+    
     return 0;
 }
